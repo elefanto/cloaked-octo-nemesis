a99f57d6aaaa1bd792f49ebdc5b1a75293ed8dbc
Roman Marukhnyak <yellowhalibut@gmail.com>
2015-04-17 09:45:57 +0300

    Create func.h

---
+++ func.h
@@ -0,0 +1,27 @@
+#ifndef FUNC_HPP
+#define FUNC_HPP
+
+class Arr {
+
+	private:
+		const static size_t step = 10;
+		size_t size;
+		double *ptr;
+		void change_size(size_t);
+
+	public:
+		Arr();
+		Arr(int arraySize);
+		inline ~Arr(){ delete[] ptr; }
+
+		inline int 	get_size(){ return size; }
+		void 	get_element();
+		void 	get_array();
+		void 	set_element();
+		void 	set_array();
+		void	delete_element();
+		void	push_element();
+
+};
+
+#endif
