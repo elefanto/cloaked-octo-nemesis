8a1a434440c29305aea49c52d3b5dbd145bd3505
Roman Marukhnyak <yellowhalibut@gmail.com>
2015-04-27 11:47:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,42 @@
-// Demonstrates typedef keyword
 #include <iostream>
+#include "func.h"
 
 using namespace std;
 
-int fib(int);
-int b = 0;
-
 int main() {
-	int a = 10;
 
-	cout << fib(a) << endl;
-	cout << b << endl;
+	Arr test(0);
+	test.push(1);
+	test.push(2);
+	test.push(3);
+	test.push(4);
+	test.push(5);
+	test.push(6);
+	test.push(7);
+	test.push(8);
+	test.push(9);
+	test.push(10);
+	test.push(11);
+	test.get_array();
+	test.insert(3, 555.555);
+	test.get_array();
+	//	test.delete_element(3);
+	//	test.pop();
+	test.get_array();
+
+	cout << test.get_capacity() << "\t" << test.get_size() << endl;
+
+	Arr test1 = test;
+	test1.get_array();
+
+	const Arr test2 = test1;
+	test2.get_array();
+
+	cout << test.get_capacity() << "\t" << test.get_size() << endl;
+	cout << test[1] << endl;
+	const size_t bla = 3;
+	cout << test[bla] << endl;
+
 	return 0;
 }
 
-int fib(int x){
-	b++;
-	if (x < 3){
-		return 1;
-	}else{
-		return fib(x-1)+fib(x-2);
-	}
-}
