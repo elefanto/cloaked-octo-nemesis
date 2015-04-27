6dc46fc6d4cd5aa7a888fbb07b37f2e5db5888da
Roman Marukhnyak <yellowhalibut@gmail.com>
2015-04-27 11:45:59 +0300

    Update func.cpp

--- func.cpp
+++ func.cpp
@@ -1,127 +1,117 @@
 #include <iostream>
 #include <cstdlib>
+
 #include "func.h"
 
 using namespace std;
 
-Arr::Arr() {
-	size = step;
-	ptr = new double[size];
+Arr::Arr() :
+	size(0), capacity(step), ptr(new double[capacity]) {
+	for (size_t i = 0; i < capacity; ++i)
+		ptr[i] = 0;
+}
+
+Arr::Arr(size_t arraySize) :
+	size(0), capacity(arraySize), ptr(new double[arraySize]) {
 	for (size_t i = 0; i < size; ++i)
 		ptr[i] = 0;
 }
 
-Arr::Arr(size_t arraySize) {
-	size = (arraySize > 0 ? arraySize : step);
-	ptr = new double[size];
+Arr& Arr::operator=(const Arr& rhs) {
+	Arr temp(rhs);
+	swap(temp, *this);
+	return *this;
+}
 
-	for (size_t i = 0; i < size; i++)
+Arr::Arr(const Arr &rhs) :
+	size(rhs.size), capacity(rhs.capacity), ptr(new double[capacity]) {
+	for (size_t i = 0; i < size; ++i) {
+		ptr[i] = rhs.ptr[i];
+	}
+}
+
+void Arr::push(double value) {
+	if (size >= capacity) {
+		capacity += step;
+		change_size(capacity);
+	}
+	ptr[size] = value;
+	++size;
+}
+void Arr::pop() {
+	if (size > 0) {
+		ptr[size - 1] = 0;
+		--size;
+	}
+}
+void Arr::clear() {
+	for (size_t i = 0; i < size; ++i) {
 		ptr[i] = 0;
-}
-
-void Arr::get_element() {
-	size_t position;
-	cout << "Enter position(less than " << size << "):" << endl;
-	cin >> position;
-	if (position < size) {
-		cout << ptr[position] << endl;
-	} else {
-		cout << "Position is to big. GAME OVER;" << endl;
 	}
+	size = 0;
 }
 
-void Arr::set_element() {
-	size_t position;
-	double value;
+double Arr::get_element(size_t position) const {
+	assert(position < size);
+	return ptr[position];
 
-	cout << "Enter position" << endl;
-	cin >> position;
-	cout << "Enter value" << endl;
-	cin >> value;
+}
+
+void Arr::insert(size_t position, double value) {
 	if (position >= size) {
-		size_t size_old = size;
-		size = position / step * step + step; // збільшення розміру
-		change_size(size_old);
-
-	}
-	ptr[position] = value;
-}
-void Arr::set_array() {
-	cout << "Enter array's values (0 is an end):" << endl;
-	size_t count = 0, size_old = 0;
-	double input_var = 1.0;
-	do {
-		cin >> input_var;
-		if (input_var != 0) {
-			count++;
-			if (count < size && size - count > step) { // зменшення розміру
-				size = count / step * step + step;
-				size_old = count;
-				change_size(size_old);
-			}
-			if (count > size) { // збільшення розміру
-				size += step;
-				size_old = count;
-				change_size(size_old);
-			}
-			ptr[count - 1] = input_var;
+		push(value);
+	} else {
+		if (size >= capacity) {
+			capacity += step;
+			change_size(capacity);
 		}
-	} while (input_var != 0);
+		++size;
+		for (size_t i = size; i > position; --i) {
+			ptr[i] = ptr[i - 1];
+		}
+		ptr[position] = value;
+	}
 }
 
-void Arr::get_array() {
+void Arr::get_array() const {
 	cout << "Array:\t";
-	for (size_t i = 0; i < size; i++) {
+	for (size_t i = 0; i < size; ++i) {
 		cout << "[" << ptr[i] << "]";
 	}
 	cout << std::endl;
 }
 
-void Arr::delete_element() {
-	size_t position;
-	cout << "Enter position(less than " << size << "):" << endl;
-	cin >> position;
-	if (position < size) {
-		for (size_t i = position; i < size; i++) {
-			ptr[i] = ptr[i + 1];
-		}
-		ptr[size - 1] = 0;
-	} else {
-		cout << "Position is to big. GAME OVER;" << endl;
+void Arr::delete_element(size_t position) {
+	assert(position < size);
+	--size;
+	for (size_t i = position; i < size; ++i) {
+		ptr[i] = ptr[i + 1];
 	}
 }
 
-void Arr::push_element() {
-	double value;
-	size_t position = size, size_old;
-	cout << "Enter value:" << endl;
-	cin >> value;
-	for (size_t i = 0; i < size; i++) {
-		if (ptr[i] == 0) {
-			position = i;
-			break;
-		}
-	}
-	if (position == size) {
-		size_old = size;
-		size += step;
-		change_size(size_old);
-	}
-	ptr[position] = value;
-}
-
-void Arr::change_size(size_t count) {
-	double * temp_arr = new double[size];
+void Arr::change_size(size_t capacity) {
+	double *temp_arr = new double[capacity];
 	for (size_t i = 0; i < size; ++i) {
-		temp_arr[i] = 0;
-	}
-	for (size_t i = 0; i < count; ++i) {
 		temp_arr[i] = ptr[i];
 	}
-	delete[] ptr;
-	ptr = new double[size];
-	for (size_t i = 0; i < size; ++i) {
-		ptr[i] = temp_arr[i];
+	for (size_t i = size; i < capacity; ++i) {
+		temp_arr[i] = 0;
 	}
-	delete[] temp_arr;
+	delete[] ptr;
+	ptr = temp_arr;
+}
+
+void Arr::swap(Arr& lhs, Arr& rhs) {
+	size_t temp_size = rhs.size;
+	rhs.size = lhs.size;
+	lhs.size = temp_size;
+
+
+	size_t temp_capacity = rhs.capacity;
+	rhs.capacity = lhs.capacity;
+	lhs.capacity = temp_capacity;
+
+	double* temp_ptr = rhs.ptr;
+	rhs.ptr = lhs.ptr;
+	lhs.ptr = temp_ptr;
 }
