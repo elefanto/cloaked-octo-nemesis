05b8126b9726296b6bde983c329f73380a4b40de
Roman Marukhnyak <yellowhalibut@gmail.com>
2015-04-27 11:46:35 +0300

    Update func.h

--- func.h
+++ func.h
@@ -1,26 +1,57 @@
 #ifndef FUNC_HPP
 #define FUNC_HPP
 
+#include <cassert>
+
 class Arr {
 
-	private:
-		const static size_t step = 10;
-		size_t size;
-		double *ptr;
-		void change_size(size_t);
+private:
+	const static size_t step = 10;
+	size_t size, capacity;
+	double *ptr;
 
-	public:
-		Arr();
-		Arr(size_t arraySize);
-		inline ~Arr(){ delete[] ptr; }
+	void change_size(size_t);
+	void swap(Arr& lhs, Arr& rhs);
 
-		inline int 	get_size(){ return size; }
-		void 	get_element();
-		void 	get_array();
-		void 	set_element();
-		void 	set_array();
-		void	delete_element();
-		void	push_element();
+public:
+	Arr();
+	Arr(size_t arraySize);
+	Arr(const Arr &rhs);
+	inline ~Arr() {
+		delete[] ptr;
+	}
+
+	inline size_t get_size() const {
+		return size;
+	}
+	inline size_t get_capacity() const {
+		return capacity;
+	}
+	double get_element(size_t position) const;
+	void get_array() const;
+
+	void push(double value);
+	void insert(size_t position, double value);
+
+	void delete_element(size_t position);
+	void pop();
+	void clear();
+
+	Arr& operator=(const Arr& rhs);
+
+	const Arr& operator=(const Arr& rhs);
+
+	inline const double operator[](size_t position) const {
+		assert(position < size);
+		assert(ptr);
+		return get_element(position);
+	}
+
+	inline double operator[](size_t position) {
+		assert(position < size);
+		assert(ptr);
+		return get_element(position);
+	}
 
 };
 
